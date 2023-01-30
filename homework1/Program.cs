a411518f66432d485cc7f60a16369764bbaae1da
veromax3 <veromax3@gmail.com>
2023-01-30 06:04:43 +0300

    added task 8

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -73,6 +73,8 @@ else
 
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
+/*
+
 Console.Write("Input a number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine ("Your number is: " + number);
@@ -81,9 +83,39 @@ int remainder = number % 2;
 
 if(remainder == 0)
 {
-    Console.WriteLine("Your number is even");
+    Console.WriteLine("Yes, your number is even");
 }
 else
 {
-    Console.WriteLine("Your number is odd");
+    Console.WriteLine("No, your number is odd");
+}
+
+*/
+
+
+
+
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your numbers is: " + number);
+
+int CurrentNumber = 1;
+
+while (CurrentNumber <= number)
+{   
+    int remainder = CurrentNumber % 2;
+
+    if (remainder == 0)
+    {
+        Console.Write(CurrentNumber + ", ");
+        CurrentNumber = CurrentNumber + 1;
+    }
+    else
+    {
+       CurrentNumber = CurrentNumber + 1;
+    }
 }
