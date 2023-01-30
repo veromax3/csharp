678a9fde70552b30aea7352c33631ab7449bbb54
veromax3 <veromax3@gmail.com>
2023-01-30 05:19:38 +0300

    added task 4

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
+/*
+
 Console.Write("Input the first number: ");
 int FirstNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("The first number is: " + FirstNumber);
@@ -15,5 +17,47 @@ if(FirstNumber > SecondNumber)
 else
 {
     Console.Write("The first number is less than the second number");
+}
+
+*/
+
+
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.Write("Input the first number: ");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("The first number is: " + FirstNumber);
+
+Console.Write("Input the second number: ");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("The second number is: " + SecondNumber);
+
+Console.Write("Input the third number: ");
+int ThirdNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("The third number is: " + ThirdNumber);
+
+int MaxNumber;
+
+if(FirstNumber > SecondNumber)
+{
+    MaxNumber = FirstNumber;
+}
+
+else 
+{
+    MaxNumber = SecondNumber;
+}
+
+if (MaxNumber < ThirdNumber)
+{
+    MaxNumber = ThirdNumber;
+    Console.Write("The maximum number is " + MaxNumber);
+}
+else 
+{
+    Console.Write("The maximum number is " + MaxNumber);
 }
 
