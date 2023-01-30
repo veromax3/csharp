bad6602c08a0f5d8117fec4a7a38f5aeb49085e5
veromax3 <veromax3@gmail.com>
2023-01-30 06:14:12 +0300

    completed task

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -1,7 +1,5 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-/*
-
 Console.Write("Input the first number: ");
 int FirstNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("The first number is: " + FirstNumber);
@@ -12,14 +10,13 @@ Console.WriteLine("The second number is: " + SecondNumber);
 
 if(FirstNumber > SecondNumber)
 {
-    Console.Write("The first number is greater than the second number");
+    Console.WriteLine("The first number is greater than the second number");
 }
 else
 {
-    Console.Write("The first number is less than the second number");
+    Console.WriteLine("The first number is less than the second number");
 }
 
-*/
 
 
 
@@ -28,43 +25,40 @@ else
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
 
-/*
-
 Console.Write("Input the first number: ");
-int FirstNumber = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("The first number is: " + FirstNumber);
+int FirstNumber1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("The first number is: " + FirstNumber1);
 
 Console.Write("Input the second number: ");
-int SecondNumber = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("The second number is: " + SecondNumber);
+int SecondNumber2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("The second number is: " + SecondNumber2);
 
 Console.Write("Input the third number: ");
-int ThirdNumber = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("The third number is: " + ThirdNumber);
+int ThirdNumber3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("The third number is: " + ThirdNumber3);
 
 int MaxNumber;
 
-if(FirstNumber > SecondNumber)
+if(FirstNumber1 > SecondNumber2)
 {
-    MaxNumber = FirstNumber;
+    MaxNumber = FirstNumber1;
 }
 
 else 
 {
-    MaxNumber = SecondNumber;
+    MaxNumber = SecondNumber2;
 }
 
-if (MaxNumber < ThirdNumber)
+if (MaxNumber < ThirdNumber3)
 {
-    MaxNumber = ThirdNumber;
-    Console.Write("The maximum number is " + MaxNumber);
+    MaxNumber = ThirdNumber3;
+    Console.WriteLine("The maximum number is " + MaxNumber);
 }
 else 
 {
-    Console.Write("The maximum number is " + MaxNumber);
+    Console.WriteLine("The maximum number is " + MaxNumber);
 }
 
-*/
 
 
 
@@ -72,8 +66,6 @@ else
 
 
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-
-/*
 
 Console.Write("Input a number: ");
 int number = Convert.ToInt32(Console.ReadLine());
@@ -90,7 +82,6 @@ else
     Console.WriteLine("No, your number is odd");
 }
 
-*/
 
 
 
@@ -100,16 +91,16 @@ else
 // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 Console.Write("Input a number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Your numbers is: " + number);
+int Number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your numbers is: " + Number);
 
 int CurrentNumber = 1;
 
-while (CurrentNumber <= number)
+while (CurrentNumber <= Number)
 {   
-    int remainder = CurrentNumber % 2;
+    int Remainder = CurrentNumber % 2;
 
-    if (remainder == 0)
+    if (Remainder == 0)
     {
         Console.Write(CurrentNumber + ", ");
         CurrentNumber = CurrentNumber + 1;
