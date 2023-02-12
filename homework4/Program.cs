63cdae91443259372adff71153225450eb05ec41
veromax3 <veromax3@gmail.com>
2023-02-12 06:05:43 +0300

    added task 27

--- homework4/Program.cs
+++ homework4/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-
+/*
 int RaiseToPower(int A, int B)
 {
     int result = 1;
@@ -32,3 +32,24 @@ int numB = Convert.ToInt32(Console.ReadLine());
 
 int raise = RaiseToPower(numA, numB);
 Console.WriteLine($"The result of raising number {numA} to the power {numB} is: {raise}");
+
+*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Sum(int Number)
+{
+    int sum = 0;
+    while (Number > 0)
+    {
+        sum  = sum + Number % 10;
+        Number = Number / 10;
+    }
+    return sum; 
+}
+
+Console.WriteLine("Input a number: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumElements = Sum (Number);
+Console.WriteLine($"Sum of elements of number {Number} is: {SumNumElements}");
