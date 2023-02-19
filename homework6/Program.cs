43dc4049abace24938dcd71a83bc740d7f5f7fb4
veromax3 <veromax3@gmail.com>
2023-02-19 06:47:33 +0300

    added task 41

---
+++ homework6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Input number of elements: ");
+int NumEl = Convert.ToInt32(Console.ReadLine());
+int counter = 0;
+for(int i = 0; i < NumEl; i++)
+{
+    Console.WriteLine($"Input {i + 1} element: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num > 0)
+        counter++; 
+}
+Console.WriteLine($"The number of elements entered by the user is {counter}");
