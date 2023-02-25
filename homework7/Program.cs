c3f6978a5788000ce68795fc13373bfc6210b347
veromax3 <veromax3@gmail.com>
2023-02-25 09:35:57 +0300

    added task 52

--- homework7/Program.cs
+++ homework7/Program.cs
@@ -34,7 +34,7 @@ Show2dArray(MyArray);
 
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и
 // возвращает значение этого элемента или же указание, что такого элемента нет.
-
+/*
 int[,] CreateRandom2dArray ()
 {
     int rows = new Random().Next(0, 20 + 1);
@@ -79,3 +79,65 @@ void findNumber(int[,] array)
 int[,] MyArray = CreateRandom2dArray();
 findNumber(MyArray);
 Show2dArray(MyArray);
+*/
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] CreateRandom2DArray ()
+{
+    Console.WriteLine("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input a number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+    int[,] array = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+void Show2DArray (int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void ShowArray (double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(Math.Round(array[i], 1) + " ");
+    Console.WriteLine();
+}
+
+
+double[] Average(int[,] array)
+{
+    
+    double[] arrayMean = new double[array.GetLength(1)];
+
+    for(int ii = 0, j = 0; j < array.GetLength(1) && ii < array.GetLength(1); ii++, j++)
+    {
+        double mean = 0;
+        for(int i = 0; i < array.GetLength(0); i++)
+        {
+            mean = (array[i, j] + mean);
+            
+        }
+        arrayMean[ii] = (mean / array.GetLength(0));
+    } 
+    return arrayMean;
+}
+
+
+int[,] myArray = CreateRandom2DArray();
+Show2DArray(myArray);
+double[] average = Average(myArray);
+ShowArray(average);
